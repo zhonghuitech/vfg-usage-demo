0d2bb779b8f2fd07f2fcdca5a4069e0d4507cf28
aborn <loveaborn@foxmail.com>
2024-10-12 21:46:33 +0800

    vfg 0.03

--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'; // 三方依赖
+import 'element-plus/theme-chalk/index.css';
+import * as ElIcon from '@element-plus/icons-vue'
+import { VueDraggableNext } from "vue-draggable-next";
+import DraggableWarp from "@/components/VFG/form/DraggableWarp.vue";
+import OptionInput from "@/components/VFG/form/OptionInput.vue";
+import UploadWarp from "@/components/VFG/form/elementWarp/UploadWarp.vue";
 
-createApp(App).mount('#app')
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/googlecode.css' //样式文件
+
+import { VFG } from "vfg"
+
+const app = createApp(App);
+app.use(ElementPlus, {
+    locale: zhCn,
+});
+
+app.use(VFG)
+app.component("draggable", VueDraggableNext);
+app.component("draggable-warp", DraggableWarp);
+app.component("option-input", OptionInput);
+Object.keys(ElIcon).forEach((key) => {
+    app.component(key, ElIcon[key])
+})
+app.component("upload-warp", UploadWarp);
+
+app.directive('highlight', function (el) {
+    let blocks = el.querySelectorAll('pre code');
+    setTimeout(() => {
+        blocks.forEach((block) => {
+            hljs.highlightBlock(block)
+        })
+    }, 200)
+})
+app.mount('#app')
