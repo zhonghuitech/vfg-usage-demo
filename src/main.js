33066a8c644981c675a3503acd929689da7674a4
aborn <loveaborn@foxmail.com>
2024-10-12 21:54:22 +0800

    0.0.4

--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import ElementPlus from 'element-plus'; // 三方依赖
 import 'element-plus/theme-chalk/index.css';
 import * as ElIcon from '@element-plus/icons-vue'
 import { VueDraggableNext } from "vue-draggable-next";
-import DraggableWarp from "@/components/VFG/form/DraggableWarp.vue";
-import OptionInput from "@/components/VFG/form/OptionInput.vue";
-import UploadWarp from "@/components/VFG/form/elementWarp/UploadWarp.vue";
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/googlecode.css' //样式文件
 
-import { VFG, SvgIcon } from "vfg"
+import { VFG, SvgIcon, DraggableWarp, OptionInput, UploadWarp } from "vfg"
 
 const app = createApp(App);
 app.use(ElementPlus, {
