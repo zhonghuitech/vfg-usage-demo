cafa8393bb962d60531bdba2261656390a472203
aborn <loveaborn@foxmail.com>
2024-10-15 09:00:03 +0800

    vfg 0.05

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'; // 三方依赖
 import 'element-plus/theme-chalk/index.css';
 import * as ElIcon from '@element-plus/icons-vue'
-import { VueDraggableNext } from "vue-draggable-next";
+import { VueDraggable } from 'vue-draggable-plus'
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/googlecode.css' //样式文件
 
-import { VFG, SvgIcon, DraggableWarp, OptionInput, UploadWarp } from "vfg"
+import { VFG, SvgIcon, OptionInput, UploadWrap } from "vfg"
 import 'vfg/dist/style.css';
 
 const app = createApp(App);
@@ -19,14 +19,13 @@ app.use(ElementPlus, {
 
 app.use(VFG)
 app.component("svg-icon", SvgIcon);
-app.component("draggable", VueDraggableNext);
-app.component("draggable-warp", DraggableWarp);
+app.component("draggable", VueDraggable);
 app.component("option-input", OptionInput);
 import 'virtual:svg-icons-register'
 Object.keys(ElIcon).forEach((key) => {
     app.component(key, ElIcon[key])
 })
-app.component("upload-warp", UploadWarp);
+app.component("upload-warp", UploadWrap);
 
 app.directive('highlight', function (el) {
     let blocks = el.querySelectorAll('pre code');
